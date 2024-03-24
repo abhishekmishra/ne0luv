c9ceae48c40c72a9275d200aa06b0f72bfa2faef
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-25 00:57:59 +0530

    change box size to proportion of parent.

--- main.lua
+++ main.lua
@@ -16,16 +16,16 @@ function love.load()
     )
     for i = 1, 3 do
         local lp = Layout(
-            Rect(0, 0, 100, 100),
+            Rect(0, 0, top:getWidth(), top:getHeight()/3.0),
             {
                 bgColor = { 0, 0, 0.2 * i, 1 }
             }
         )
         for j = 1, 3 do
             local sp = Layout(
-                Rect(0, 0, 33, 33),
+                Rect(0, 0, lp:getWidth()/3, lp:getHeight()),
                 {
-                    bgColor = { 0, 0.2 * j, 0, 1 }
+                    bgColor = { 0.2 * i, 0.2 * j, 0, 1 }
                 }
             )
             lp:addChild(sp)
