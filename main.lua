12924802b53dbaa661633741a8a83b2b831f774e
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-25 10:37:43 +0530

    slider control and its value added for all panels in the grid

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@
 local Layout = require('layout')
 local Rect = require('rect')
 local Text = require('text')
+local Slider = require('slider')
 
 local top
 
@@ -16,30 +17,56 @@ function love.load()
         topConfig
     )
     for i = 1, 3 do
-        local lp = Layout(
+        local outerPanel = Layout(
             Rect(0, 0, top:getWidth(), top:getHeight() / 3.0),
             {
                 bgColor = { 0, 0, 0.2 * i, 1 }
             }
         )
         for j = 1, 3 do
-            local sp = Layout(
-                Rect(0, 0, lp:getWidth() / 3, lp:getHeight()),
+            local innerPanel = Layout(
+                Rect(0, 0, outerPanel:getWidth() / 3, outerPanel:getHeight()),
                 {
+                    layout = 'column',
                     bgColor = { 0.2 * i, 0.2 * j, 0, 1 }
                 }
             )
             local txt = Text(
-                Rect(0, 0, sp:getWidth(), sp:getHeight()),
+                Rect(0, 0, innerPanel:getWidth(), innerPanel:getHeight() / 3),
                 {
                     fgColor = { 1, 1, 1, 1 },
                     text = 'Text ' .. i .. ' ' .. j,
                     font = love.graphics.newFont(24)
-                })
-            sp:addChild(txt)
-            lp:addChild(sp)
+                }
+            )
+            local slider = Slider(
+                Rect(0, 0, innerPanel:getWidth(), innerPanel:getHeight() / 3),
+                {
+                    minValue = 0,
+                    maxValue = 100,
+                    currentValue = 50,
+                    bgColor = { 0.2 * i, 0.2 * j, 0, 1 }
+                }
+            )
+            local sliderValue = Text(
+                Rect(0, 0, innerPanel:getWidth(), innerPanel:getHeight() / 3),
+                {
+                    fgColor = { 1, 1, 1, 1 },
+                    text = 'Value: ' .. slider.currentValue,
+                    font = love.graphics.newFont(24)
+                }
+            )
+
+            slider:addChangeHandler(function(value)
+                sliderValue:setText('Value: ' .. value)
+            end)
+
+            innerPanel:addChild(txt)
+            innerPanel:addChild(slider)
+            innerPanel:addChild(sliderValue)
+            outerPanel:addChild(innerPanel)
         end
-        top:addChild(lp)
+        top:addChild(outerPanel)
     end
     top:show()
 end
@@ -53,4 +80,20 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+end
+
+function love.update(dt)
+    top:update(dt)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    top:mousepressed(x, y, button, istouch, presses)
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    top:mousereleased(x, y, button, istouch, presses)
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    top:mousemoved(x, y, dx, dy, istouch)
 end
--- slider.lua
+++ slider.lua
@@ -13,13 +13,14 @@ local Panel = require('Panel')
 Slider = class('Slider', Panel)
 
 -- Constructor for the Slider class
-function Slider:initialize(dim, minValue, maxValue, currentValue)
-    Panel.initialize(self, dim)
-    self.minValue = minValue or 0                     -- Default minValue is 0
-    self.maxValue = maxValue or 100                   -- Default maxValue is 100
-    self.currentValue = currentValue or self.minValue -- Default currentValue is minValue
+function Slider:initialize(rect, config)
+    Panel.initialize(self, rect)
+    self.config = config or {}
+    self.minValue = self.config.minValue or 0                     -- Default minValue is 0
+    self.maxValue = self.config.maxValue or 100                   -- Default maxValue is 100
+    self.currentValue = self.config.currentValue or self.minValue -- Default currentValue is minValue
     self.handleWidth = 10                             -- Width of the handle
-    self.handleHeight = self.dim.h                    -- Height of the handle
+    self.handleHeight = self:getHeight()                    -- Height of the handle
     self.handleX = self:calculateHandlePosition()     -- X position of the handle
     self.changeHandler = {}
 end
@@ -28,13 +29,13 @@ end
 function Slider:calculateHandlePosition()
     local range = self.maxValue - self.minValue
     local fraction = (self.currentValue - self.minValue) / range
-    return fraction * (self.dim.w - self.handleWidth)
+    return self:getX() + fraction * (self:getWidth() - self.handleWidth)
 end
 
 -- Method to update the current value based on the handle position
 function Slider:updateCurrentValue()
     local range = self.maxValue - self.minValue
-    local fraction = (self.handleX) / (self.dim.w - self.handleWidth)
+    local fraction = (self.handleX - self:getX()) / (self:getWidth() - self.handleWidth)
     self.currentValue = self.minValue + fraction * range
     self.currentValue = math.floor(self.currentValue + 0.5)
     self:fireChangeHandlers()
@@ -64,17 +65,17 @@ end
 function Slider:_draw()
     love.graphics.push()
 
-    -- Draw the line
-    love.graphics.line(0, self.dim.h / 2, self.dim.w, self.dim.h / 2)
-
     if self.dragging then
         love.graphics.setColor(0.5, 0.5, 0.5, 1)
     else
         love.graphics.setColor(0.8, 0.8, 0.8, 1)
     end
 
+    -- Draw the line
+    love.graphics.line(self:getX(), self:getY() + self:getHeight() / 2, self:getX() + self:getWidth(), self:getY() + self:getHeight() / 2)
+
     -- Draw the handle
-    love.graphics.rectangle('fill', self.handleX, 0, self.handleWidth, self.handleHeight)
+    love.graphics.rectangle('fill', self.handleX, self:getY(), self.handleWidth, self.handleHeight)
 
     love.graphics.pop()
 end
@@ -84,7 +85,7 @@ function Slider:_mousepressed(x, y, button)
     -- print("Slider:mousepressed [" .. x .. ", " .. y .. "]")
     if button == 1 and x >= self.handleX
         and x <= self.handleX + self.handleWidth
-        and y >= 0 and y <= self.handleHeight then
+        and y >= self:getY() and y <= self:getY() + self.handleHeight then
         self.dragging = true
     else
         self.handleX = x
@@ -98,8 +99,8 @@ function Slider:_mousemoved(x, y, dx, dy)
     -- print("Slider:mousemoved")
     if self.dragging then
         self.handleX = self.handleX + dx
-        self.handleX = math.max(0,
-            math.min(self.dim.w - self.handleWidth, self.handleX))
+        self.handleX = math.max(self:getX(),
+            math.min(self:getX() + self:getWidth() - self.handleWidth, self.handleX))
         self:updateCurrentValue()
     end
 end
@@ -111,5 +112,17 @@ function Slider:_mousereleased(x, y, button)
         self.dragging = false
     end
 end
+
+--- Override the setX method to update the handle position when
+-- the x position of the slider is changed
+function Slider:setX(x)
+    self.rect:setX(x)
+    self.handleX = self:calculateHandlePosition()
+end
+
+-- function Slider:setY(y)
+--     self.rect:setY(y)
+-- end
+
 
 return Slider
