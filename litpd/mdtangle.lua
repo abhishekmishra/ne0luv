b54644ae0b2b51af47bbb481b768dd52486faf7d
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-26 12:20:50 +0530

    updated litpd to include fix to ignore code blocks without code_file attribute.

--- litpd/mdtangle.lua
+++ litpd/mdtangle.lua
@@ -4,17 +4,19 @@
 -- license: MIT see LICENSE file
 -- date: 21/03/2024
 -- author: Abhishek Mishra
+
 local tangle = {}
 
+
 local function get_file_name (code_block)
-  local file_name = code_block.attributes["code_file"]
-  if file_name == nil then
-    file_name = "Untitled.lua"
-  end
-  return file_name
+  return code_block.attributes["code_file"]
 end
+
 local function get_file (code_block)
   local full_path = get_file_name(code_block)
+  if full_path == nil then
+    return nil, nil
+  end
   local file = io.open(full_path, "a")
   return full_path, file
 end
@@ -31,6 +33,9 @@ end
 
 function tangle.CodeBlock (code_block)
   local full_path, file = get_file(code_block)
+  if full_path == nil then
+    return
+  end
   print("Tangling code block at " .. full_path)
   write_code_block(code_block, file)
   close_file(file)
