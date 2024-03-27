bd880c388b9932dcfdda9828a9db9cfacb6a58e4
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-27 15:53:58 +0530

    initial steps to turn the entire ne0luv package into one literate program.

--- build.lua
+++ build.lua
@@ -11,6 +11,9 @@ local Class = require('middleclass')
 -- global Module list
 local MODULES = {}
 
+-- global Document list
+local DOCUMENTS = {}
+
 -- src folder
 local SRC_FOLDER = 'src/'
 
@@ -28,19 +31,15 @@ local Module = Class('Module')
 -- Create a new module definition and register it with the global MODULES table.
 -- @param config A table containing the name of the module, the litpd file, the code file and the html file.
 function Module:initialize(config)
-    self.name = config[1]
-    self.litpdFile = SRC_FOLDER .. config[2]
-    self.codeFile = config[3]
-    self.htmlFile = OUTPUT_FOLDER .. config[4]
+    self.name = config.name
+    self.codeFile = config.codeFile
 
     -- register the module as next in the list
     table.insert(MODULES, self)
 end
 
---- Module:generate
--- Generate the module file from the litpd file.
-function Module:generate()
-    -- clean the dist folder by removing the code and html files
+function Module:clean()
+    -- clean the dist folder by removing the code
     local cmd = 'rm -f ' .. OUTPUT_FOLDER .. self.codeFile
     print('Executing: ' .. cmd)
     local handle = io.popen(cmd)
@@ -49,28 +48,13 @@ function Module:generate()
         return
     end
     handle:close()
+end
 
-    cmd = 'rm -f ' .. self.htmlFile
-    print('Executing: ' .. cmd)
-    local handle = io.popen(cmd)
-    if handle == nil then
-        print('Error executing command')
-        return
-    end
-    handle:close()
-
-    -- run the litpd program to generate the code and html files
-    cmd = 'lua ./litpd/litpd.lua ' .. self.litpdFile .. ' --to=html --standalone --toc --output=' .. self.htmlFile
-    print('Executing: ' .. cmd)
-    local handle = io.popen(cmd)
-    if handle == nil then
-        print('Error executing command')
-        return
-    end
-    handle:close()
-
+--- Module:generate
+-- Generate the module file from the litpd file.
+function Module:generate()
     -- move the code file to the output folder
-    cmd = 'mv ' .. self.codeFile .. ' ' .. OUTPUT_FOLDER
+    local cmd = 'mv ' .. self.codeFile .. ' ' .. OUTPUT_FOLDER
     print('Executing: ' .. cmd)
     local handle = io.popen(cmd)
     if handle == nil then
@@ -86,97 +70,193 @@ end
 
 --- Module:toString
 function Module:toString()
-    return 'Module: ' .. self.name .. ' (' .. self.litpdFile .. ' -> ' .. self.codeFile .. ', ' .. self.htmlFile .. ')'
+    return 'Module: ' .. self.name .. ' (' .. self.codeFile .. ')'
 end
 
 -- create the modules
-Module {'Vector', 'vector.md', 'vector.lua', 'vector.html'}
-Module {'Rect', 'rect.md', 'rect.lua', 'rect.html'}
-Module {'Panel', 'panel.md', 'panel.lua', 'panel.html'}
-Module {'Text', 'text.md', 'text.lua', 'text.html'}
-Module {'Button', 'button.md', 'button.lua', 'button.html'}
-Module {'Slider', 'slider.md', 'slider.lua', 'slider.html'}
-Module {'Layout', 'layout.md', 'layout.lua', 'layout.html'}
+-- Module {'Vector', 'vector.md', 'vector.lua', 'vector.html'}
+-- Module {'Rect', 'rect.md', 'rect.lua', 'rect.html'}
+-- Module {'Panel', 'panel.md', 'panel.lua', 'panel.html'}
+-- Module {'Text', 'text.md', 'text.lua', 'text.html'}
+-- Module {'Button', 'button.md', 'button.lua', 'button.html'}
+-- Module {'Slider', 'slider.md', 'slider.lua', 'slider.html'}
+-- Module {'Layout', 'layout.md', 'layout.lua', 'layout.html'}
 
+--- A document class which represents the main literate program in the ne0luv
+-- library.
 
--- generate the modules
-for _, module in pairs(MODULES) do
-    module:generate()
+local Document = Class('Document')
+
+--- Document:initialize
+-- Create a new document definition and register it with the global DOCUMENTS table.
+-- @param config A table containing the name of the document, the litpd file,
+-- and the associated modules.
+function Document:initialize(config)
+    self.config = config
+    self.name = self.config.name
+    self.litpdFile = SRC_FOLDER .. self.config.litpd
+    self.htmlFile = OUTPUT_FOLDER .. self.name .. '.html'
+    self.modules = {}
+    for _, c in ipairs(self.config.modules) do
+        table.insert(self.modules, Module(c))
+    end
+
+    -- register the document as next in the list
+    table.insert(DOCUMENTS, self)
 end
 
--- Open the output file
-local output = io.open(NE0LUV_FILE, 'w')
-
-if not output then
-    print('Error: Could not open output file at ' .. NE0LUV_FILE)
-    return
+--- Document:__tostring
+function Document:__tostring()
+    return 'Document: ' .. self.name .. ' (' .. self.litpdFile .. ' -> ' .. self.htmlFile .. ')'
 end
 
--- Write the header
-output:write('--[[\n')
-output:write('  ne0luv - Some love2d utilities\n')
-output:write('\n')
-output:write('  date: 25/03/2024\n')
-output:write('  author: Abhishek Mishra\n')
-output:write(']]\n')
-
--- Write the module loader
-output:write('local modules = {}\n')
-output:write('\n')
-
--- Write the modules
-for _, moduleDef in ipairs(MODULES) do
-    local moduleName = moduleDef.name
-    local moduleFile = moduleDef.codeFile
-
-    print('Merging ' .. moduleName .. ' from ' .. moduleFile)
-
-    local file = io.open(moduleFile, 'r')
-
-    if not file then
-        print('Error: Could not open module file at ' .. moduleFile)
+--- Document:clean
+-- Clean the document by removing the generated html file.
+function Document:clean()
+    -- clean the dist folder by removing the html file
+    local cmd = 'rm -f ' .. self.htmlFile
+    print('Executing: ' .. cmd)
+    local handle = io.popen(cmd)
+    if handle == nil then
+        print('Error executing command')
         return
     end
+    handle:close()
 
-    output:write('\n')
-    output:write('-- ' .. moduleName .. '\n')
-    output:write('\n')
-
-    local content = file:read('*a')
-    -- replace "return moduleName" with ""
-    content = content:gsub('return ' .. moduleName .. '%s+', '')
-    -- replace any line which requires a module in the moduleSequence with an empty line.
-    for _, mdef in ipairs(MODULES) do
-        local m = mdef.name
-        content = content:gsub('local ' .. m .. ' = require%(\'' .. m:lower() .. '\'%)', '')
+    -- clean the modules
+    for _, module in pairs(self.modules) do
+        module:clean()
     end
-    output:write(content)
 
-    file:close()
+    print('Cleaned ' .. self:__tostring())
 end
 
--- Load the modules into the modules table
-for _, moduleDef in ipairs(MODULES) do
-    local module = moduleDef.name
-    output:write('modules["' .. module .. '"] = ' .. module .. '\n')
+--- Document:generate
+-- Generate the document file from the litpd file.
+function Document:generate()
+    -- run the litpd program to generate the html file
+    local cmd = 'lua ./litpd/litpd.lua ' .. self.litpdFile .. ' --to=html --standalone --toc --output=' .. self.htmlFile
+    print('Executing: ' .. cmd)
+    local handle = io.popen(cmd)
+    if handle == nil then
+        print('Error executing command')
+        return
+    end
+    handle:close()
+
+    -- generate the modules
+    for _, module in pairs(self.modules) do
+        module:generate()
+    end
+
+    print('Generated ' .. self:__tostring())
 end
 
--- Return the modules table
-output:write('return modules\n')
+Document {
+    name = 'ne0luv',
+    litpd = 'ne0luv.md',
+    modules = {
+        { name = 'Vector', codeFile = 'vector.lua' },
+        -- { name = 'Rect', codeFile = 'rect.lua' },
+        -- { name = 'Panel', codeFile = 'panel.lua' },
+        -- { name = 'Text', codeFile = 'text.lua' },
+        -- { name = 'Button', codeFile = 'button.lua' },
+        -- { name = 'Slider', codeFile = 'slider.lua' },
+        -- { name = 'Layout', codeFile = 'layout.lua' }
+    }
+}
 
--- Close the output file
-output:close()
-
-print('Merged modules into ' .. NE0LUV_FILE)
-
--- copy middleclass.lua to dist folder
-local cmd = 'cp middleclass.lua ' .. OUTPUT_FOLDER
-print('Executing: ' .. cmd)
-local handle = io.popen(cmd)
-if handle == nil then
-    print('Error executing command')
-    return
+for _, doc in pairs(DOCUMENTS) do
+    print(doc)
+    doc:clean()
+    doc:generate()
 end
-handle:close()
+-- Module {'Vector', 'vector.md', 'vector.lua', 'vector.html'}
+-- Module {'Rect', 'rect.md', 'rect.lua', 'rect.html'}
+-- Module {'Panel', 'panel.md', 'panel.lua', 'panel.html'}
+-- Module {'Text', 'text.md', 'text.lua', 'text.html'}
+-- Module {'Button', 'button.md', 'button.lua', 'button.html'}
+-- Module {'Slider', 'slider.md', 'slider.lua', 'slider.html'}
+-- Module {'Layout', 'layout.md', 'layout.lua', 'layout.html'}
 
-print('Done!')
+-- -- generate the modules
+-- for _, module in pairs(MODULES) do
+--     module:generate()
+-- end
+
+-- -- Open the output file
+-- local output = io.open(NE0LUV_FILE, 'w')
+
+-- if not output then
+--     print('Error: Could not open output file at ' .. NE0LUV_FILE)
+--     return
+-- end
+
+-- -- Write the header
+-- output:write('--[[\n')
+-- output:write('  ne0luv - Some love2d utilities\n')
+-- output:write('\n')
+-- output:write('  date: 25/03/2024\n')
+-- output:write('  author: Abhishek Mishra\n')
+-- output:write(']]\n')
+
+-- -- Write the module loader
+-- output:write('local modules = {}\n')
+-- output:write('\n')
+
+-- -- Write the modules
+-- for _, moduleDef in ipairs(MODULES) do
+--     local moduleName = moduleDef.name
+--     local moduleFile = moduleDef.codeFile
+
+--     print('Merging ' .. moduleName .. ' from ' .. moduleFile)
+
+--     local file = io.open(moduleFile, 'r')
+
+--     if not file then
+--         print('Error: Could not open module file at ' .. moduleFile)
+--         return
+--     end
+
+--     output:write('\n')
+--     output:write('-- ' .. moduleName .. '\n')
+--     output:write('\n')
+
+--     local content = file:read('*a')
+--     -- replace "return moduleName" with ""
+--     content = content:gsub('return ' .. moduleName .. '%s+', '')
+--     -- replace any line which requires a module in the moduleSequence with an empty line.
+--     for _, mdef in ipairs(MODULES) do
+--         local m = mdef.name
+--         content = content:gsub('local ' .. m .. ' = require%(\'' .. m:lower() .. '\'%)', '')
+--     end
+--     output:write(content)
+
+--     file:close()
+-- end
+
+-- -- Load the modules into the modules table
+-- for _, moduleDef in ipairs(MODULES) do
+--     local module = moduleDef.name
+--     output:write('modules["' .. module .. '"] = ' .. module .. '\n')
+-- end
+
+-- -- Return the modules table
+-- output:write('return modules\n')
+
+-- -- Close the output file
+-- output:close()
+
+-- print('Merged modules into ' .. NE0LUV_FILE)
+
+-- -- copy middleclass.lua to dist folder
+-- local cmd = 'cp middleclass.lua ' .. OUTPUT_FOLDER
+-- print('Executing: ' .. cmd)
+-- local handle = io.popen(cmd)
+-- if handle == nil then
+--     print('Error executing command')
+--     return
+-- end
+-- handle:close()
+
+-- print('Done!')
